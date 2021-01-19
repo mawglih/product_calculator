a451f57b7d008604e79359ec2cf1eacc977e0792
Oleg Markoff <mawglih@gmail.com>
2021-01-19 17:16:17 -0500

    upd3

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// start calculator
 const productIds = {am: '77412', bem: '77412', an: '77431', ben: '77431', ao: '77441', beo: '77441', bep: 'no product', bfm: '77428',
 cem: '77428', bfn: '77432', cen: '77432', bfo: '77442', ceo: '77442', bfp: '77445', cep: '77445', cfm: '77429', dem: '77429', cfn: '77433', den: '77433', cfo: '77443', deo: '77443', cfp: '77446', dep: '77446', dfm: '77430', dfn: '77440', dfo: '77444', dfp: '77447'};
 const radioRow1 = [{label:'Речь не развита по возрасту', value:'e', name: 'row1'}, {label:'Речь развита по возрасту', value:'f', name:'row1'},];
@@ -9,7 +10,10 @@ const doMagic = () => {
   const resultTitle = document.createElement('h1');
   const resultValue = document.createElement('span');
   let sum = '';
+  console.log('sum cleared2', sum);
   sum = calculateResult();
+  console.log('sum after calc', sum);
+  console.log('product', productIds[sum])
   resultTitle.innerHTML='Вам нужно до хрена прикупить!';
   resultValue.innerHTML=`[products columns=3 ids="${productIds[sum]}"]`;
   result.appendChild(resultTitle);
@@ -66,17 +70,16 @@ const removeChildren = (id, remove) => {
 };
 
 const calculateResult = () => {
-  let elem = document.getElementsByTagName('input');
-  let sum = '';
+  let elem = document.getElementById('container-calc').getElementsByTagName('input');
+  let prodId = '';
   for(i=0; i<elem.length; i++) {
-    if(elem[i].type='radio') {
-      if(elem[i].checked) {
-        sum += elem[i].value;
-      }
+    if((elem[i].type='radio') && (elem[i].checked)) {
+      prodId += elem[i].value;
+      console.log('prodId:' , prodId);
     }
   }
-  console.log('sum before return', sum);
-  return sum;
+  console.log('sum before return', prodId);
+  return prodId;
 };
 
 function createRadio(id1, id2, array, fn, id3, id4, array2) {
@@ -88,3 +91,4 @@ function createRadio1(id1, id2, arr) {
   console.log('executed1');
   getRowCreated(id1, id2, arr, true, true);
 };
+// end calculator
